a9d33c49615f9bff4cc045d347d08f2a7b4f6c93
Mereta Helin <mehelin@student.jyu.fi>
2021-11-05 17:35:18 +0200

    Muutettu taas juttuja.

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,11 +26,14 @@ module.exports = {
             'never'
         ],
         'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-        'error', 'always'],
-        'arrow-spacing': [
-        'error', { 'before': true, 'after': true }],
-        'no-console': 0,
-    },
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+        'error', 'always'
+    ],
+    'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0,
+  },
+    
 }
