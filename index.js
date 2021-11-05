f29006f8ea8141664291b724bd98d126f0ec85e4
Mereta Helin <mehelin@student.jyu.fi>
2021-11-05 16:47:06 +0200

    Muutoksi

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.use(unknownEndpoint)
 // virheellisten pyyntöjen käsittely
 app.use(errorHandler)
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
